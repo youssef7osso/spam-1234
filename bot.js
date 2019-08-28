5f4d1c0edf61969cf79b624376ab25a8b968ac53
youssef7osso <54089843+youssef7osso@users.noreply.github.com>
2019-08-28 05:40:25 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("611799502440890368ا")
+setInterval(function() {
+channel.send(`@ProBot ✨#5803 `);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
